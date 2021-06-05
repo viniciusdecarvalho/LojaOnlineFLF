254c294f00896b9588dae050436a4f702e421e51
Vinicius de Carvalho <viniciusdecarvalho.tec@gmail.com>
2021-06-05 15:36:44 -0300

    suporte a compressao gzip

---
+++ LojaOnlineFLF.WebAPI/StartupResponseCompressionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using LojaOnlineFLF.DataModel.Providers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace LojaOnlineFLF.WebAPI
+{
+    public static class StartupResponseCompressionExtensions
+    {
+        public static IApplicationBuilder UseCompressionConfig(this IApplicationBuilder app)
+        {
+            app.UseResponseCompression();
+
+            return app;
+        }
+
+        public static IServiceCollection AddCompressionConfig(this IServiceCollection services)
+        {
+            services.AddResponseCompression(config =>
+            {
+                config.Providers.Add<GzipCompressionProvider>();
+            })
+            .Configure<GzipCompressionProviderOptions>(opt =>
+            {
+                opt.Level = System.IO.Compression.CompressionLevel.Fastest;
+            });
+
+            return services;
+        }
+    }
+}
