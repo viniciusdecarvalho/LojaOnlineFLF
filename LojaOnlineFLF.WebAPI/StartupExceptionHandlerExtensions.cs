272a723263812f379a2ce8875e71fe3db99963b6
Vinicius de Carvalho <viniciusdecarvalho.tec@gmail.com>
2021-06-06 00:13:25 -0300

    melhoria nas respostas de falha

--- LojaOnlineFLF.WebAPI/StartupExceptionHandlerExtensions.cs
+++ LojaOnlineFLF.WebAPI/StartupExceptionHandlerExtensions.cs
@@ -1,21 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Text;
-using FluentValidation;
-using GlobalExceptionHandler.WebApi;
-using LojaOnlineFLF.DataModel;
-using LojaOnlineFLF.DataModel.Models;
-using LojaOnlineFLF.WebAPI.Services;
-using LojaOnlineFLF.WebAPI.Services.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 namespace LojaOnlineFLF.WebAPI
@@ -24,45 +12,26 @@ namespace LojaOnlineFLF.WebAPI
     {        
         public static IApplicationBuilder UseExceptionHandlerConfig(this IApplicationBuilder app)
         {
-            // app.UseGlobalExceptionHandler(x => {
-            //     x.ContentType = K.MediaTypes.AplicationJson;
-            //     x.ResponseBody(e => {
-            //         var problemDetails = new Microsoft.AspNetCore.Mvc.ProblemDetails
-            //         {
-            //             Status = StatusCodes.Status403Forbidden,
-            //             Type = "https://example.com/probs/out-of-credit",
-            //             Title = "Division by zero...",
-            //             Detail = ex.StackTrace,
-            //             Instance = HttpContext.Request.Path
-            //         };
-
-            //         return JsonConvert.SerializeObject(problemDetails);
-            //     });
-
-            //     //x.Map<RecordNotFoundException>().ToStatusCode(StatusCodes.Status404NotFound);
-            //     x.Map<Exception>().ToStatusCode(StatusCodes.Status400BadRequest);
-            // });
-
             app.UseExceptionHandler(builder =>
             {
                 builder.Run(async context =>
                 {
-                    var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var feature = context.Features.Get<IExceptionHandlerFeature>();
 
-                    if (exceptionHandlerFeature != null)
+                    if (feature != null)
                     {
-                        var exception = exceptionHandlerFeature.Error;
-                        
+                        var exception = feature.Error;
+
                         var problemDetails = new ProblemDetails
                         {
                             Title = exception.Message,
                             Status = StatusCodes.Status400BadRequest,
-                            Detail = exception.StackMessage(),
-                            Instance = context.Request.Path                        
+                            Detail = exception.Messages(),
+                            Instance = context.Request.Path
                         };
 
                         var json = JsonConvert.SerializeObject(problemDetails);
-                        
+
                         context.Response.StatusCode = problemDetails.Status.Value;
                         context.Response.ContentType = K.MediaTypes.AplicationProblemJson;
                         await context.Response.WriteAsync(json);
@@ -73,11 +42,11 @@ namespace LojaOnlineFLF.WebAPI
             return app;
         }
 
-        public static string StackMessage(this Exception exception)
+        public static string Messages(this Exception exception)
         {
             StringBuilder builder = new StringBuilder();
 
-            var ex = exception;
+            var ex = exception?.InnerException;
 
             while (ex != null)
             {
