2f98727b1e2e0af3f07a0c14b599d9b3837a9e83
Vinicius de Carvalho <viniciusdecarvalho.tec@gmail.com>
2021-06-06 00:15:52 -0300

    encapsular transformacao entre entidades e view

---
+++ LojaOnlineFLF.WebAPI/Services/IMapperService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using AutoMapper;
+using LojaOnlineFLF.DataModel.Models;
+
+namespace LojaOnlineFLF.WebAPI.Services
+{
+    public interface IMapperService
+    {
+        TDestination Map<TDestination>(object source);
+
+        TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
+    }
+}
