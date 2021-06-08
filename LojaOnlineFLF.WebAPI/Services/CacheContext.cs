38ba4638a9f6bc58d29f6fe1e15c1edeab58a0df
Vinicius de Carvalho <viniciusdecarvalho.tec@gmail.com>
2021-06-08 01:40:34 -0300

    suporte jwt com refresh token

---
+++ LojaOnlineFLF.WebAPI/Services/CacheContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaOnlineFLF.WebAPI.Services
+{
+    internal class CacheContext: DbContext
+    {
+        public CacheContext(DbContextOptions<CacheContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Cache>()
+                .HasKey(x => x.Chave);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Cache> Cache { get; set; }
+    }
+
+    public class Cache
+    {
+        public string Chave { get; set; }
+
+        public string Valor { get; set; }
+
+        public string Tipo { get; set; }
+    }
+}
+
